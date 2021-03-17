2bfacce78af53ddbff13291121539e63524cd40a
Kiataro <neverspaik@gmail.com>
2021-03-17 10:53:26 +0300

    first commit

---
+++ WebAPIFeedback/ClientApp/src/app/app.component.ts
@@ -0,0 +1,73 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpService } from './http.service';
+import { Post } from './post';
+import { PostFromDB } from './postFromDb';
+
+@Component({
+    selector: 'app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    providers: [HttpService]
+})
+
+export class AppComponent implements OnInit {
+
+    myForm: FormGroup;
+    post: Post = new Post();
+    receivedPost: PostFromDB;
+    done: boolean = false;
+  
+    topics: Array<string> = [
+        "Техподдержка", 
+        "Продажи", 
+        "Другое",
+    ];
+    constructor(private httpService: HttpService) { }
+
+    ngOnInit(): void {
+ 
+        this.myForm = new FormGroup({
+            nameUser: new FormControl(
+                null,
+                [
+                Validators.required
+                ]),
+            emailUser: new FormControl(
+                null,
+                [
+                Validators.email,
+                Validators.required
+                ]),
+            telUser: new FormControl(
+                null,
+                [
+                    Validators.required
+                ]),
+            topic: new FormControl(
+                "",
+                [
+                    Validators.required
+                ]),
+            textUser: new FormControl(
+                null,
+                [
+                    Validators.required
+                ])
+        });
+    }
+
+   
+
+    onChange(topicValue) {
+        this.post.topic = topicValue;
+    }
+
+    submit(post) {
+            this.httpService.postData(post)
+                .subscribe(
+                    (data: PostFromDB) => { this.receivedPost = data; this.done = true; },
+                    error => console.log(error)
+                );
+    }
+}
